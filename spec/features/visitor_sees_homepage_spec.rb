80dcb7396f26b3a103a05a990463e1c9d5bbea96
jtrtj <jt.r@live.com>
2018-09-07 09:32:53 -0600

    the homepage finally has some style!

--- spec/features/visitor_sees_homepage_spec.rb
+++ spec/features/visitor_sees_homepage_spec.rb
@@ -5,7 +5,7 @@ describe 'a visitor' do
     it 'will see a navbar, link to sign up, and a recent vote' do
       visit '/'
 
-      expect(page).to have_css('.nav')
+      expect(page).to have_css('.navbar')
       expect(page).to have_css('.recent-voter')
       expect(page).to have_link('sign up')
       expect(page).to have_link('log in')
