7e09da31715df82f0209f0d7fdc14fdf833959fa
jtrtj <jt.r@live.com>
2018-09-06 18:31:44 -0600

    add test for homepage

---
+++ spec/features/visitor_sees_homepage_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'a visitor' do
+  context 'navigating to / ' do
+    it 'will see a navbar, link to sign up, and a recent vote' do
+      visit '/'
+
+      expect(page).to have_css('.nav')
+      expect(page).to have_css('.recent-voter')
+      expect(page).to have_link('sign up')
+      expect(page).to have_link('log in')
+    end
+  end
+end
