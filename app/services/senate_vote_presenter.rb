9d87d3b7ca5db80fd060abfe6806bddf0a2ff835
jtrtj <jt.r@live.com>
2018-09-07 19:54:17 -0600

    change view a little to represent what will be coming back, services are passing their tests

--- app/services/senate_vote_presenter.rb
+++ app/services/senate_vote_presenter.rb
@@ -5,13 +5,18 @@ class SenateVotePresenter
   end
 
   def most_recent
-    fetch_recent_votes.first
+    votes.first
   end
+
+  def votes
+    fetch_recent_votes.map do |vote|
+      SenateVote.new(vote)
+    end
+  end
+
+  private
 
   def fetch_recent_votes
     @recent_votes ||= FetchVotesService.fetch_senate_votes
-    @recent_votes.map do |vote|
-      SenateVote.new(vote)
-    end
   end
 end
--- spec/services/fetch_vote_service_spec.rb
+++ spec/services/fetch_vote_service_spec.rb
@@ -4,14 +4,14 @@ describe FetchVotesService do
   context 'class methods' do
     it '.fetch_senate_votes - returns an array of hashes containing vote data' do
       vote_data = FetchVotesService.fetch_senate_votes
-
-      expect(vote_data).to have_key(:question)
-      expect(vote_data).to have_key(:description)
-      expect(vote_data).to have_key(:result)
-      expect(vote_data[:total]).to have_key(:yes)
-      expect(vote_data[:total]).to have_key(:no)
-      expect(vote_data[:total]).to have_key(:present)
-      expect(vote_data[:total]).to have_key(:not_voting)
+      
+      expect(vote_data.first).to have_key(:question)
+      expect(vote_data.last).to have_key(:description)
+      expect(vote_data.first).to have_key(:result)
+      expect(vote_data.first[:total]).to have_key(:yes)
+      expect(vote_data.last[:total]).to have_key(:no)
+      expect(vote_data.first[:total]).to have_key(:present)
+      expect(vote_data.last[:total]).to have_key(:not_voting)
     end
   end
 end
