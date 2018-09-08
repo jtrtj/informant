1c96cd9a739cad72ff509c5c8fb311ea6781b8eb
jtrtj <jt.r@live.com>
2018-09-07 19:32:10 -0600

    pass propublica connection test

--- spec/services/pro_publica_connection_spec.rb
+++ spec/services/pro_publica_connection_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 describe ProPublicaConnection do
   context 'class methods' do
     it '.fetch(params) - makes a git request with provided params' do
-      response = ProPublicaConnection.fetch('congress/v1/senate/votes/recent.json')
-      expect(response["status"]).to eq("OK")
-      expect(response).to have_key("results")
+      conn = ProPublicaConnection.conn
+      response = conn.get('congress/v1/senate/votes/recent.json')
+      parsed_response_body = JSON.parse(response.body)
+
+      expect(parsed_response_body["status"]).to eq("OK")
+      expect(parsed_response_body).to have_key("results")
     end
   end
 end
