1ba90bb228c846c8b97458356d31bad0d36e902d
jtrtj <jt.r@live.com>
2018-09-11 09:53:51 -0600

    all tests green

--- spec/features/visitor_sees_homepage_spec.rb
+++ spec/features/visitor_sees_homepage_spec.rb
@@ -4,7 +4,7 @@ describe 'a visitor' do
   context 'navigating to / ' do
     it 'will see a navbar, link to sign up, and a recent vote', :vcr do
       visit '/'
-
+      
       expect(page).to have_css('.navbar')
       expect(page).to have_css('.recent-vote')
       expect(page).to have_button('sign up')
