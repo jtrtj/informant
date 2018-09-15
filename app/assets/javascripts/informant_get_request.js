a17ce0085149a71354a81bbb33da62ce460d080e
jtrtj <jt.r@live.com>
2018-09-15 16:01:34 -0600

    start making js fetch happen on homepage

---
+++ app/assets/javascripts/informant_get_request.js
@@ -0,0 +1,48 @@
+const url = 'http://localhost:3000/api/v1/most-recent-vote'
+
+fetch(url)
+  .then(response => response.json())
+  .then(vote => {
+    console.log(vote)
+    // debugger;
+    var voteData = vote[0]
+    var voteTitle = document.querySelector(".vote-date-title")
+    voteTitle.innerHTML = `On ${voteData.created_at} The U.S. Senated Voted`
+    var voteQuestion = document.querySelector(".vote-question")
+    voteQuestion.innerHTML = voteData.question
+    var voteDescription = document.querySelector(".vote-description")
+    voteDescription.innerHTML = voteData.description
+
+    var voteArticleCards = document.querySelector(".vote-article-cards")
+    var voteArticles = voteData.articles
+    var articleCards = voteArticles.map(article => {
+
+      return (
+        `<div class="card horizontal">
+        
+        ${article.title}
+        </div>`
+      )
+     
+    })
+    $(".vote-article-cards").html(articleCards)
+
+
+
+    // var recentVote = document.querySelector('.recent-vote')
+    // var allVotes = votes.data
+    // allVotes.map(vote => {
+    //   var voteElement = document.createElement('div')
+    //   voteElement.setAttribute('class', 'display-3')
+    //   voteElement.innerHTML = `
+    //     <h5 class="display-3 vote-question">${vote.question}</h5>
+    //     <h3 class="card-text">
+    //       ${vote.description}
+    //     </h3>
+    //   `
+    //   recentVote.prepend(voteElement)
+    // })
+  })
+
+
+
