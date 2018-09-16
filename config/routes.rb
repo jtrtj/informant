b160d091d1e7bebf665d0ec3b07705be079ac40b
jtrtj <jt.r@live.com>
2018-09-15 19:24:45 -0600

    add dashbaord route and redirect on session create

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
  get root to: 'welcome#index'
  get '/auth/:privder/callback', to: 'sessions#create'
  get '/logout', to: 'sessions#destroy'
+ get '/dashboard', to: 'dashboard#index'
 end
