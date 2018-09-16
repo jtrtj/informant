391a00f178ce15bba613a71c5c4b80d547f97181
jtrtj <jt.r@live.com>
2018-09-15 18:32:15 -0600

    add find or update method to user and add destroy action to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,17 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
-    sessions[:user_id] = @user.id
+    session[:user_id] = @user.id
     redirect_to root_path
   end
 
+  def destroy
+    if current_user
+      session.delete[:user_id]
+      flash[:success] = "You have been logged out."
+    end
+    redirect_to root_path
+  end
   protected
 
   def auth_hash
